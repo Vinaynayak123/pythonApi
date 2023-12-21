870d846eb83dabc481c7d3a5eca1efd39857de2a
Vinay Nayak <154420080+Vinaynayak123@users.noreply.github.com>
2023-12-21 13:24:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,40 +1,36 @@
-from flask import Flask, request, jsonify
 import os
 import shutil
 
-app = Flask(__name__)
+# Directories to be searched for temporary files
+temp_directories = [
+    '/private/var/folders',
+    '~/Library/Caches',
+    '~/Library/Logs',
+    '~/Downloads',
+    '~/Desktop'
+]
 
-def clean_junk_files(directory):
-    # Define the file extensions or criteria for identifying junk files
-    junk_file_extensions = ['.tmp', '.bak', '.log', '.swp', '.DS_Store']
+# List of file extensions to be removed
+extensions = [
+    '.log',
+    '.cache',
+    '.tmp',
+    '.dmg',
+    '.pkg'
+]
 
-    # Iterate through files in the directory and remove junk files
-    for root, dirs, files in os.walk(directory):
-        for file in files:
-            if any(file.endswith(ext) for ext in junk_file_extensions):
-                file_path = os.path.join(root, file)
-                os.remove(file_path)
-
-    return {"message": "Junk files cleaned successfully"}
-
-@app.route('/clean-junk-files', methods=['POST'])
-def clean_junk_files_api():
-    data = request.get_json()
-
-    # Check if the 'directory' key is present in the JSON request
-    if 'directory' not in data:
-        return jsonify({"error": "Missing 'directory' parameter"}), 400
-
-    directory = data['directory']
-
-    # Check if the specified directory exists
-    if not os.path.exists(directory):
-        return jsonify({"error": "Directory not found"}), 404
-
-    # Perform the junk file cleaning operation
-    result = clean_junk_files(directory)
-
-    return jsonify(result)
-
-if _name_ == '__main__':
-    app.run(debug=True)
+for directory in temp_directories:
+    for dirpath, dirnames, filenames in os.walk(os.path.expanduser(directory)):
+        for filename in filenames:
+            # Check if the file extension is in the list of extensions to be removed
+            if os.path.splitext(filename)[1].lower() in extensions:
+                filepath = os.path.join(dirpath, filename)
+                try:
+                    if os.path.isfile(filepath):
+                        os.remove(filepath)
+                    elif os.path.isdir(filepath):
+                        shutil.rmtree(filepath)
+                    print(f"Removed {filepath}")
+                except Exception as e:
+                    print(f"Error deleting {filepath}: {e}")
+                    
